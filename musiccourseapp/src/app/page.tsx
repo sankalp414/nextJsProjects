490133f4eb6fb026ab3adaa439aa1ca9735980ee
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2024-02-28 10:00:50 +0000

    next

---
+++ musiccourseapp/src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="text-2xl text-center">Chai aur code</h1>
+     
+  );
+}
---
+++ musiccourseapp/src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+'use client';
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+const Navbar = ({ className }: { className?: string }) => {
+    const [active, setActive] = useState<string | null>(null);
+    return (
+        <>
+            <div
+                className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+            ><Menu setActive={setActive}>
+
+                    <MenuItem setActive={setActive} active={active} item="Home">
+                        <div className="flex flex-col space-y-4 text-sm">
+                            <HoveredLink href="/web-dev">Web Development</HoveredLink>
+                            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+                            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+                            <HoveredLink href="/branding">Branding</HoveredLink>
+                        </div>
+                    </MenuItem>
+
+                    <MenuItem setActive={setActive} active={active} item="Our Courses">
+                        <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+                            <div className="flex flex-col space-y-4 text-sm">
+                                <HoveredLink href="/web-dev">Web Development</HoveredLink>
+                                <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+                                <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+                                <HoveredLink href="/branding">Branding</HoveredLink>
+                            </div>
+                        </div>
+                    </MenuItem>
+                    <MenuItem setActive={setActive} active={active} item="Pricing">
+                        <div className="flex flex-col space-y-4 text-sm">
+                            <HoveredLink href="/hobby">Hobby</HoveredLink>
+                            <HoveredLink href="/individual">Individual</HoveredLink>
+                            <HoveredLink href="/team">Team</HoveredLink>
+                            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+                        </div>
+                    </MenuItem>
+                </Menu></div>
+
+        </>
+    )
+}
+
+export default Navbar
